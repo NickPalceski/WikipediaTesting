e88dff385212eaa7498dc37df4e7a26660ee672f
Daniel J. Lowry <117498727+danieljlowry@users.noreply.github.com>
2025-04-21 14:03:33 -0400

    Update and rename GeneralHomePageTest.java to MiscellaneousTest.java
    Updated test to match class 8

---
+++ src/main/java/org/WikiTest/MiscellaneousTest.java
@@ -0,0 +1,32 @@
+package org.WikiTest;
+
+import org.testng.annotations.Test;
+
+public class MiscellaneousTest {
+
+// 8.1 - Navigate to random article
+    @Test(priority = 1)
+    public void navigateToRandomArticle() {
+    }
+
+// 8.2 -  Donate page
+    @Test(priority = 2)
+    public void donatePage() {
+    }
+
+// 8.3 -  Navigate to Sister Projects
+    @Test(priority = 3)
+    public void navigateToSisterProjects() {
+    }
+
+// 8.4 - Reference Desk
+    @Test(priority = 4)
+    public void referenceDesk() {
+    }
+
+// 8.5 - Editing Talk Page
+    @Test(priority = 5)
+    public void editingTalkPage() {
+    }
+
+}
