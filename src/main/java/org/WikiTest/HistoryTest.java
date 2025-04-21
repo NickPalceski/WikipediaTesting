1f3fbb06ef0c7460d54288ccca9aa329fc1cd03c
Daniel J. Lowry <117498727+danieljlowry@users.noreply.github.com>
2025-04-21 14:12:29 -0400

    Update HistoryTest.java
    Changes the task numbers to match class 7

--- src/main/java/org/WikiTest/HistoryTest.java
+++ src/main/java/org/WikiTest/HistoryTest.java
@@ -4,27 +4,27 @@ import org.testng.annotations.Test;
 
 public class HistoryTest {
 
-// 8.1 - Filter Revisions
+// 7.1 - Filter Revisions
     @Test(priority = 1)
     public void filterRevisions() {
     }
 
-// 8.2 - Compare Webpage Versions
+// 7.2 - Compare Webpage Versions
     @Test(priority = 2)
     public void compareWebpageVersions() {
     }
 
-// 8.3 - User Contribution Search
+// 7.3 - User Contribution Search
     @Test(priority = 3)
     public void userContributionSearch() {
     }
 
-// 8.4 - Move through different versions
+// 7.4 - Move through different versions
     @Test(priority = 4)
     public void moveThroughDifferentVersions() {
     }
 
-// 8.5 - Pageviews Analysis
+// 7.5 - Pageviews Analysis
     @Test(priority = 5)
     public void pageviewsAnalysis() {
     }
