890157db04fba9832aa6cfedc71bd82c35fd6f85
Daniel J. Lowry <117498727+danieljlowry@users.noreply.github.com>
2025-04-21 13:25:39 -0400

    Update and rename FeaturesTest.java to PreferencesTest.java
    Updated test to match class 4

---
+++ src/main/java/org/WikiTest/PreferencesTest.java
@@ -0,0 +1,32 @@
+package org.WikiTest;
+
+import org.testng.annotations.Test;
+
+public class PreferencesTest {
+
+// 4.1 - Appearance Tab
+    @Test(priority = 1)
+    public void appearanceTab() {
+    }
+
+// 4.2 - Editing Tab
+    @Test(priority = 2)
+    public void editingTab() {
+    }
+
+// 4.3 - Recent Changes Tab
+    @Test(priority = 3)
+    public void recentChangesTab() {
+    }
+
+// 4.4 - Search Tab
+    @Test(priority = 4)
+    public void searchTab() {
+    }
+
+// 4.5 - Reset All Settings
+    @Test(priority = 5)
+    public void resetAllSettings() {
+    }
+
+}
