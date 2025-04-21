85723a8828d19ec56f7c118794259129fd9ae060
Daniel J. Lowry <117498727+danieljlowry@users.noreply.github.com>
2025-04-21 13:31:05 -0400

    Update and rename ViewHistoryTest.java to AccessibilityTest.java
    Updated test to match class 6

---
+++ src/main/java/org/WikiTest/AccessibilityTest.java
@@ -0,0 +1,32 @@
+package org.WikiTest;
+
+import org.testng.annotations.Test;
+
+public class AccessibilityTest {
+
+// 6.1 - Language Selector
+    @Test(priority = 1)
+    public void languageSelector() {
+    }
+
+// 6.2 - Right to Left Languages
+    @Test(priority = 2)
+    public void rightToLeftLangauges() {
+    }
+
+// 6.3 - Alt Text
+    @Test(priority = 3)
+    public void altText() {
+    }
+
+// 6.4 - Keyboard Shortcuts
+    @Test(priority = 4)
+    public void keyboardShortcuts() {
+    }
+
+// 6.5 - Responsive Design
+    @Test(priority = 5)
+    public void responsiveDesign() {
+    }
+
+}
