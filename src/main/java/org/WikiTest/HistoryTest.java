5092af396451791c7f115e14386e75e5d8b6ec2a
Daniel J. Lowry <117498727+danieljlowry@users.noreply.github.com>
2025-04-21 13:33:52 -0400

    Update and rename DonatePageTest.java to HistoryTest.java
    Updated test to match class 7

---
+++ src/main/java/org/WikiTest/HistoryTest.java
@@ -0,0 +1,32 @@
+package org.WikiTest;
+
+import org.testng.annotations.Test;
+
+public class HistoryTest {
+
+// 8.1 - Filter Revisions
+    @Test(priority = 1)
+    public void filterRevisions() {
+    }
+
+// 8.2 - Compare Webpage Versions
+    @Test(priority = 2)
+    public void compareWebpageVersions() {
+    }
+
+// 8.3 - User Contribution Search
+    @Test(priority = 3)
+    public void userContributionSearch() {
+    }
+
+// 8.4 - Move through different versions
+    @Test(priority = 4)
+    public void moveThroughDifferentVersions() {
+    }
+
+// 8.5 - Pageviews Analysis
+    @Test(priority = 5)
+    public void pageviewsAnalysis() {
+    }
+
+}
